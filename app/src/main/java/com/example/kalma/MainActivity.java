b0a968f16de75c85969a825256d24a5007b52df2
unknown <asmak9060@gamil.com>
2021-07-03 23:57:19 +0500

    3rd commit

--- app/src/main/java/com/example/kalma/MainActivity.java
+++ app/src/main/java/com/example/kalma/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.kalma;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview = findViewById(R.id.kalma);
+        String[] pdfFileNames = {"Pehla Kalma", "Dusra Kalma", "Teesra Kalma", "Chotha Kalma", "Panchwa Kalma", "Chata Kalma"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pdfFileNames) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                return view;
+            }
+        };
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = listview.getItemAtPosition(position).toString();
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("fileName",item);
+                startActivity(intent);
+            }
+        });
     }
 }
