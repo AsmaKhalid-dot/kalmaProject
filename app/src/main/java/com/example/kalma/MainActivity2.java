36ade148b809da077586f0c3e73729f2d683ed56
unknown <asmak9060@gamil.com>
2021-07-03 23:58:27 +0500

    4th commit

--- app/src/main/java/com/example/kalma/MainActivity2.java
+++ app/src/main/java/com/example/kalma/MainActivity2.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.github.barteksc.pdfviewer.PDFView;
 
+public class MainActivity2 extends AppCompatActivity {
+    PDFView pdfView;
+    //MediaPlayer med;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //int md=getResources().getIdentifier("name","raw",getPackageName());
+        //med=MediaPlayer.create(getApplicationContext(),md);
+        //med.start();
+        pdfView=(PDFView)findViewById(R.id.opener);
+        String getitem=getIntent().getStringExtra("filename");
+        if(getitem.equals("Pehla Kalma"))
+        {
+            pdfView.fromAsset("1 kalima.pdf").load();
+        }
+        else if(getitem.equals("Dusra Kalma"))
+        {
+            pdfView.fromAsset("2 kalima.pdf").load();
+        }
+        else if(getitem.equals("Teesra Kalma"))
+        {
+            pdfView.fromAsset("3 kalima.pdf").load();
+        }
+        else if(getitem.equals("Chotha Kalma"))
+        {
+            pdfView.fromAsset("4 kalima.pdf").load();
+        }
+        else if(getitem.equals("Panchwa Kalma"))
+        {
+            pdfView.fromAsset("5 kalima.pdf").load();
+        }
+        else if(getitem.equals("Chata Kalma")) {
+            pdfView.fromAsset("6 kalima.pdf").load();
+        }
     }
 }
